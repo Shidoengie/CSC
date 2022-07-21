86edfbc4d5bb8c941baa482534908dd8b8ec6562
shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-07-21 11:57:19 +0100

    publish

---
+++ CSC lexer/TokenType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC_lexer
+{
+    public enum Tokentype
+    {
+        // Single-character tokens.
+        LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+        COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+        // One or two character tokens.
+        BANG, BANG_EQUAL,
+        EQUAL, EQUAL_EQUAL,
+        GREATER, GREATER_EQUAL,
+        LESS, LESS_EQUAL,
+        LESS_LESS, PLUS_PLUS,
+
+        // Literals.
+        IDENTIFIER, STRING, NUMBER,
+
+        // Keywords.
+        AND, CLASS, ELSE, ELIF, FALSE, FUNC, FOR, IF, NIL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, CONST, WHILE,
+
+        EOF
+    }
+}
