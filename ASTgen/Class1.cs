b00284b0714a833016e123358bc8b0c842ca8851
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-08-12 13:23:20 +0100

    template class file

---
+++ ASTgen/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LOCS;
+namespace ASTgen
+{
+    class Class1
+    {
+    }
+    //template class
+    /*
+    abstract class Expr
+    {
+        public abstract void accept(Pasteryvisitor pasteryvisitor);
+        public class bread : Expr
+        {
+            public override void accept(Pasteryvisitor visitor)
+            {
+                visitor.visitbin(this);
+            }
+        }
+        public interface Pasteryvisitor
+        {
+            void visitbin(bread bread);
+        }
+    */
+    
+    
+}
+
